975f4a334bc53145f3010f3d1bd75e99b339667e
Manish Gupta <63774978+ManishGupta-x@users.noreply.github.com>
2026-02-20 21:57:06 +0530

    logger

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -23,6 +23,7 @@ class Logger {
   info(message, data) { this.log('info', message, data); }
   error(message, data) { this.log('error', message, data); }
   warn(message, data) { this.log('warn', message, data); }
+  debug(message, data) { this.log('debug', message, data); }
 }
 
 module.exports = new Logger();  
